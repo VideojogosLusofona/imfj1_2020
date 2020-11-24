69bf11aa9ac89cb99c1eb89fdbe5019f764fa812
Diogo Andrade <diogo.g.andrade@gmail.com>
2020-11-24 16:09:06 +0000

    Added matrix exercise stub

---
+++ matrix/matrix.py
@@ -0,0 +1,47 @@
+
+# This function returns an identity matrix with the given
+# degree
+def get_identity_matrix(degree):
+    pass
+
+# This function returns the dimensions (x,y) of a matrix
+# Use multiple return
+# If the given matrix is None, then return 0,0
+def get_matrix_dimension(matrix):
+    pass
+
+# This function prints the given matrix, along with the
+# size. Output should be similar to:
+# 5 x 5
+# [1, 0, 0, 0, 0]
+# [0, 1, 0, 0, 0]
+# [0, 0, 1, 0, 0]
+# [0, 0, 0, 1, 0]
+# [0, 0, 0, 0, 1]
+# If the matrix is None, then don't do anything
+def print_matrix(matrix):
+    pass
+
+# This functions returns a matrix with the given number of lines and
+# of columns, all set to zero
+def get_null_matrix(n_lines, n_columns):
+    pass
+
+# This function returns the multiplication of the given matrices.
+# The function should check for a valid input and print an error if
+# the matrixes can't be multiplied
+def matrix_multiply(a,b):
+    pass
+
+#i5 = get_identity_matrix(5)
+#print(i5)
+#print_matrix(i5)
+#m = get_null_matrix(2, 5)
+#print_matrix(m)
+#m[0][2] = 2
+#m[1][3] = 5
+#print_matrix(m)
+#c = matrix_multiply(i5,m)
+#print_matrix(c)
+#d = matrix_multiply(m,i5)
+#print_matrix(d)
